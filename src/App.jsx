2ab0e0255485abef62887bc58cd6a1d9cc0d0401
Võ Công Nam <vocongnam172@gmail.com>
2025-11-08 13:07:56 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState } from 'react';
 import "./index.css";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import AnimatedBackground from "./components/Background";
 import Navbar from "./components/Navbar";
-import Portofolio from "./Pages/Portofolio";
-import ContactPage from "./Pages/Contact";
+import Portofolio from "./pages/Portofolio";
+import ContactPage from "./pages/Contact";
 import ProjectDetails from "./components/ProjectDetail";
-import WelcomeScreen from "./Pages/WelcomeScreen";
+import WelcomeScreen from "./pages/WelcomeScreen";
 import { AnimatePresence } from 'framer-motion';
-import NotFoundPage from "./Pages/404";
-import WorkExperience from "./Pages/WorkExperience";
+import NotFoundPage from "./pages/404";
+import WorkExperience from "./pages/WorkExperience";
 
 const LandingPage = ({ showWelcome, setShowWelcome }) => {
   return (
